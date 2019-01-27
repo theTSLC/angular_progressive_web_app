4ba57b941d9bd7aa727d92bd8eb6570f67c29d51
Tom Le Coney <tleconey@gmail.com>
2019-01-27 12:53:35 -0600

    v1: generate header, footer, layout components

---
+++ store/src/app/ui/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+    <p>
+      footer works!
+    </p>
+  `,
+  styles: []
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- store/src/app/ui/ui.module.ts
+++ store/src/app/ui/ui.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './containers/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ]
