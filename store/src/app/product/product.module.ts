07f13df98e2b7737e898f3227e64663ba94f704b
Tom Le Coney <tleconey@gmail.com>
2019-01-27 16:40:53 -0600

    v1: product module and class

---
+++ store/src/app/product/product.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductRoutingModule } from './product-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductRoutingModule
+  ]
+})
+export class ProductModule { }
