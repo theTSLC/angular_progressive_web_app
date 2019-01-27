d54ce666739df7acd8d9ffd64de343e1f758d7ca
Tom Le Coney <tleconey@gmail.com>
2019-01-27 16:28:46 -0600

    v1: header construction

--- store/src/app/ui/components/header/header.component.ts
+++ store/src/app/ui/components/header/header.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <p>
-      header works!
-    </p>
+    <nav class="navbar navbar-expand navbar-dark bg-dark fixed-top">
+      <a routerLink="/" class="navbar-brand">
+        <img [attr.src]="logo" [attr.alt]="title" width="30" height="30">
+          {{title}}
+      </a>
+      <div class="collapse navbar-collapse">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link" *ngFor="let link of links" [routerLink]="link.url" routerLinkActive="active" [routerLinkActiveOptions]="{exact:true}">
+            {{link.label}}
+          </a>
+        </div>
+      </div>
+    </nav>
   `,
   styles: []
 })
 export class HeaderComponent implements OnInit {
+  public logo = 'assets/logo.svg';
+  public title= 'Store';
+  public links= [{
+    label : 'Products', 
+    url   : '/products',
+  }];
 
   constructor() { }
 
