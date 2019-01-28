d4948c27dfdd819898474b3f2289a85c49714656
Tom Le Coney <tleconey@gmail.com>
2019-01-27 21:25:11 -0600

    v1: adding express server

--- store/src/app/product/services/product.service.ts
+++ store/src/app/product/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from '../../../environments/environment.prod';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 
